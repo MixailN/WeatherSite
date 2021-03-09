f3bfa455b6d0fdd999adadcd4d01e68ca824e6bc
Michael <mixail_nabiullin@mail.ru>
2021-03-09 22:46:09 +0300

    Remade requests using XmlHttpRequest instead of fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,92 @@
 var currentCityContainer = document.getElementsByClassName("current-city-container")[0];
-API_KEY = "b55cb6a60addb3d56b8affed8e202b01";
+const API_KEY = "b55cb6a60addb3d56b8affed8e202b01";
+const form = document.getElementsByClassName("search-form");
+const input = document.getElementsByClassName("search-field");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let inputVal = input.value.toLowerCase();
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    let weather_url = `api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}`
+    xhr.open("GET", weather_url);
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status !== 200) {
+            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            let answer = xhr.response;
+            let x = document.getElementById("script-test");
+            const li = document.createElement("li");
+            li.classList.add("favorite-city");
+            const markup = `
+                <div class="favorite-city-header">
+                    <h3 class="favorite-city-name">${answer.city}</h3>
+                    <span class="favorite-temperature">Math.round(answer.main.temp)&degC</span>
+                    <img class="weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
+                    <button class="close-button">x</button>
+                </div>
+                <ul class="city-data">
+                    <li class="weather-data"><span class="title">Ветер</span> <span class="description">северно-южный ${answer.wind.speed}м/с</span></li>
+                    <li class="weather-data"><span class="title">Облачность</span> <span class="description">облачно</span></li>
+                    <li class="weather-data"><span class="title"> Давление</span><span class="description">${answer.main.pressure}гПа</span></li>
+                    <li class="weather-data"><span class="title">Влажность</span><span class="description">${answer.main.humidity}%</span></li>
+                    <li class="weather-data"><span class="title">Координаты</span><span class="description">[${Math.round(lat)}, ${Math.round(lon)}]</span></li>
+                </ul>
+            `;
+            li.innerHTML = markup;
+            let list = document.getElementsByClassName("favorites");
+            list.appendChild(li);
+        }
+        form.reset();
+        input.focus();
+    }
+});
+
+function getFavoriteCity() {
+    let input = document.getElementsByClassName("search-field");
+    let inputVal = input.value;
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    let weather_url = `api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}`
+    xhr.open("GET", weather_url);
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status !== 200) {
+            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            let answer = xhr.response;
+            let x = document.getElementById("script-test");
+            const li = document.createElement("li");
+            li.classList.add("favorite-city");
+            const markup = `
+                <div class="favorite-city-header">
+                    <h3 class="favorite-city-name">${answer.city}</h3>
+                    <span class="favorite-temperature">Math.round(answer.main.temp)&degC</span>
+                    <img class="weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
+                    <button class="close-button">x</button>
+                </div>
+                <ul class="city-data">
+                    <li class="weather-data"><span class="title">Ветер</span> <span class="description">северно-южный ${answer.wind.speed}м/с</span></li>
+                    <li class="weather-data"><span class="title">Облачность</span> <span class="description">облачно</span></li>
+                    <li class="weather-data"><span class="title"> Давление</span><span class="description">${answer.main.pressure}гПа</span></li>
+                    <li class="weather-data"><span class="title">Влажность</span><span class="description">${answer.main.humidity}%</span></li>
+                    <li class="weather-data"><span class="title">Координаты</span><span class="description">[${Math.round(lat)}, ${Math.round(lon)}]</span></li>
+                </ul>
+            `;
+            li.innerHTML = markup;
+            let list = document.getElementsByClassName("favorites");
+            list.appendChild(li);
+        }
+    };
+    xhr.onprogress = function (event) {
+        // if (event.lengthComputable) {
+        //     alert(`Получено ${event.loaded} из ${event.total} байт`);
+        // } else {
+        //     alert(`Получено ${event.loaded} байт`);
+        // }
+    };
+}
 
 function getLocation() {
     if(navigator.geolocation) {
@@ -13,14 +100,16 @@ async function showData(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let cityName = await getCityName(lat, lon);
+    console.log(cityName);
     let answer = await getWeather(lat, lon);
+    alert(answer.city);
     currentCityContainer.innerHTML = `
             <ul class="current-city">
                 <li class="current-city-name-container">
                     <h2 class="current-city-name">${cityName}</h2>
                 </li>
                 <li class="weather-icon-container">
-                    <img class="current-weather-icon" src="img/Cloud.svg">
+                    <img class="current-weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
                 </li>
                 <li class="temperature-container">
                     <span class="current-temperature">${Math.round(answer.main.temp)}&degC</span>
@@ -37,23 +126,51 @@ async function showData(position) {
 }
 
 async function getCityName(lat, lon) {
-    let city_url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=ru`;
-    let response = await fetch(city_url);
-    if(response.ok) {
-        let answer = await response.json();
-        return answer.city;
-    } else {
-        alert("Error: " + response.status);
-    }
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.responseType = "json";
+        let city_url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=ru`;
+        xhr.open("GET", city_url);
+        xhr.onload = function() {
+            if (xhr.status !== 200) {
+                alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+            } else {
+                let answer = xhr.response;
+                resolve(answer.city);
+            }
+        };
+        xhr.onprogress = function (event) {
+            // if (event.lengthComputable) {
+            //     alert(`Получено ${event.loaded} из ${event.total} байт`);
+            // } else {
+            //     alert(`Получено ${event.loaded} байт`);
+            // }
+        };
+        xhr.send();
+    });
+
 }
 
 async function getWeather(lat, lon) {
-    let weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    let response = await fetch(weather_url);
-    if(response.ok) {
-        let answer = await response.json();
-        return answer;
-    } else {
-        alert("Error: " + response.status);
-    }
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.responseType = "json";
+        let weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        xhr.open("GET", weather_url);
+        xhr.onload = function() {
+            if (xhr.status !== 200) {
+                alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+            } else {
+                resolve(xhr.response);
+            }
+        };
+        xhr.onprogress = function (event) {
+            // if (event.lengthComputable) {
+            //     alert(`Получено ${event.loaded} из ${event.total} байт`);
+            // } else {
+            //     alert(`Получено ${event.loaded} байт`);
+            // }
+        };
+        xhr.send();
+    });
 }
