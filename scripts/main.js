9abb6a97ea13cbbd7493d9b46f3e2a73bf95a47e
Michael <mixail_nabiullin@mail.ru>
2021-03-10 23:08:21 +0300

    Added favorite cities searching and removing

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
-var currentCityContainer = document.getElementsByClassName("current-city-container")[0];
+const currentCityContainer = document.getElementsByClassName("current-city-container")[0];
 const API_KEY = "b55cb6a60addb3d56b8affed8e202b01";
 const form = document.getElementsByClassName("search-form");
 const input = document.getElementsByClassName("search-field");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    let inputVal = input.value.toLowerCase();
+document.forms.search.onsubmit = function() {
+    let message = this.q.value;
+    console.log(message);
+    let weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${message}&appid=${API_KEY}&units=metric`;
     let xhr = new XMLHttpRequest();
+    var li = null;
     xhr.responseType = "json";
-    let weather_url = `api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}`
     xhr.open("GET", weather_url);
     xhr.send();
     xhr.onload = function() {
@@ -16,93 +17,95 @@ form.addEventListener("submit", e => {
             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
             let answer = xhr.response;
-            let x = document.getElementById("script-test");
-            const li = document.createElement("li");
-            li.classList.add("favorite-city");
-            const markup = `
+            li.innerHTML = `
                 <div class="favorite-city-header">
-                    <h3 class="favorite-city-name">${answer.city}</h3>
-                    <span class="favorite-temperature">Math.round(answer.main.temp)&degC</span>
+                    <h3 class="favorite-city-name">${answer.name}</h3>
+                    <span class="favorite-temperature">${Math.round(answer.main.temp)}&degC</span>
                     <img class="weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
-                    <button class="close-button">x</button>
+                    <button class="close-button" onclick="deleteCity(this)">x</button>
                 </div>
                 <ul class="city-data">
                     <li class="weather-data"><span class="title">Ветер</span> <span class="description">северно-южный ${answer.wind.speed}м/с</span></li>
                     <li class="weather-data"><span class="title">Облачность</span> <span class="description">облачно</span></li>
                     <li class="weather-data"><span class="title"> Давление</span><span class="description">${answer.main.pressure}гПа</span></li>
                     <li class="weather-data"><span class="title">Влажность</span><span class="description">${answer.main.humidity}%</span></li>
-                    <li class="weather-data"><span class="title">Координаты</span><span class="description">[${Math.round(lat)}, ${Math.round(lon)}]</span></li>
+                    <li class="weather-data"><span class="title">Координаты</span><span class="description">[]</span></li>
                 </ul>
             `;
-            li.innerHTML = markup;
             let list = document.getElementsByClassName("favorites");
-            list.appendChild(li);
+            list[0].appendChild(li);
         }
-        form.reset();
-        input.focus();
     }
-});
+    xhr.onprogress = function(event) {
+        li = document.createElement("li");
+        li.classList.add("favorite-city");
+        li.innerHTML = `<div class="favorite-city-header">
+                    <h3 class="favorite-city-name">${message}</h3>
+                    <button class="close-button" onclick="deleteCity(this)">x</button>
+                </div>`;
+    }
+    return false;
+};
 
-function getFavoriteCity() {
-    let input = document.getElementsByClassName("search-field");
-    let inputVal = input.value;
-    let xhr = new XMLHttpRequest();
-    xhr.responseType = "json";
-    let weather_url = `api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}`
-    xhr.open("GET", weather_url);
-    xhr.send();
-    xhr.onload = function() {
-        if (xhr.status !== 200) {
-            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
-        } else {
-            let answer = xhr.response;
-            let x = document.getElementById("script-test");
-            const li = document.createElement("li");
-            li.classList.add("favorite-city");
-            const markup = `
-                <div class="favorite-city-header">
-                    <h3 class="favorite-city-name">${answer.city}</h3>
-                    <span class="favorite-temperature">Math.round(answer.main.temp)&degC</span>
-                    <img class="weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
-                    <button class="close-button">x</button>
-                </div>
-                <ul class="city-data">
-                    <li class="weather-data"><span class="title">Ветер</span> <span class="description">северно-южный ${answer.wind.speed}м/с</span></li>
-                    <li class="weather-data"><span class="title">Облачность</span> <span class="description">облачно</span></li>
-                    <li class="weather-data"><span class="title"> Давление</span><span class="description">${answer.main.pressure}гПа</span></li>
-                    <li class="weather-data"><span class="title">Влажность</span><span class="description">${answer.main.humidity}%</span></li>
-                    <li class="weather-data"><span class="title">Координаты</span><span class="description">[${Math.round(lat)}, ${Math.round(lon)}]</span></li>
-                </ul>
-            `;
-            li.innerHTML = markup;
-            let list = document.getElementsByClassName("favorites");
-            list.appendChild(li);
-        }
-    };
-    xhr.onprogress = function (event) {
-        // if (event.lengthComputable) {
-        //     alert(`Получено ${event.loaded} из ${event.total} байт`);
-        // } else {
-        //     alert(`Получено ${event.loaded} байт`);
-        // }
-    };
-}
+
+// form.addEventListener("submit", e => {
+//     e.preventDefault();
+//     let inputVal = input.value.toLowerCase();
+//     let xhr = new XMLHttpRequest();
+//     xhr.responseType = "json";
+//     let weather_url = `api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_KEY}`
+//     xhr.open("GET", weather_url);
+//     xhr.send();
+//     xhr.onload = function() {
+//         if (xhr.status !== 200) {
+//             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+//         } else {
+//             let answer = xhr.response;
+//             let x = document.getElementById("script-test");
+//             const li = document.createElement("li");
+//             li.classList.add("favorite-city");
+//             const markup = `
+//                 <div class="favorite-city-header">
+//                     <h3 class="favorite-city-name">${answer.city}</h3>
+//                     <span class="favorite-temperature">Math.round(answer.main.temp)&degC</span>
+//                     <img class="weather-icon" src="http://openweathermap.org/img/wn/${answer.weather[0].icon}@2x.png">
+//                     <button class="close-button">x</button>
+//                 </div>
+//                 <ul class="city-data">
+//                     <li class="weather-data"><span class="title">Ветер</span> <span class="description">северно-южный ${answer.wind.speed}м/с</span></li>
+//                     <li class="weather-data"><span class="title">Облачность</span> <span class="description">облачно</span></li>
+//                     <li class="weather-data"><span class="title"> Давление</span><span class="description">${answer.main.pressure}гПа</span></li>
+//                     <li class="weather-data"><span class="title">Влажность</span><span class="description">${answer.main.humidity}%</span></li>
+//                     <li class="weather-data"><span class="title">Координаты</span><span class="description">[${Math.round(lat)}, ${Math.round(lon)}]</span></li>
+//                 </ul>
+//             `;
+//             li.innerHTML = markup;
+//             let list = document.getElementsByClassName("favorites");
+//             list.appendChild(li);
+//         }
+//         form.reset();
+//         input.focus();
+//     }
+// });
 
 function getLocation() {
     if(navigator.geolocation) {
-        geo = navigator.geolocation.getCurrentPosition(showData)
+        geo = navigator.geolocation.getCurrentPosition(geoSuccess, geoFailure);
     } else {
-        x.innerHTML = "Your browser doesn't support geolocation"
+        x.innerHTML = "Your browser doesn't support geolocation";
     }
 }
 
-async function showData(position) {
+async function geoSuccess(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
+    currentCityContainer.innerHTML = `
+                <div class="loading-box">
+                    <h2>Подождите данные загружаются</h2>
+                </div>
+            `;
     let cityName = await getCityName(lat, lon);
-    console.log(cityName);
     let answer = await getWeather(lat, lon);
-    alert(answer.city);
     currentCityContainer.innerHTML = `
             <ul class="current-city">
                 <li class="current-city-name-container">
@@ -140,11 +143,7 @@ async function getCityName(lat, lon) {
             }
         };
         xhr.onprogress = function (event) {
-            // if (event.lengthComputable) {
-            //     alert(`Получено ${event.loaded} из ${event.total} байт`);
-            // } else {
-            //     alert(`Получено ${event.loaded} байт`);
-            // }
+            console.log(event.loaded);
         };
         xhr.send();
     });
@@ -165,12 +164,19 @@ async function getWeather(lat, lon) {
             }
         };
         xhr.onprogress = function (event) {
-            // if (event.lengthComputable) {
-            //     alert(`Получено ${event.loaded} из ${event.total} байт`);
-            // } else {
-            //     alert(`Получено ${event.loaded} байт`);
-            // }
+            console.log(event.loaded);
         };
         xhr.send();
     });
+}
+
+function geoFailure() {
+    currentCityContainer.innerHTML = `
+        <h2>Ошибка</h2>
+    `;
+}
+
+function deleteCity(element) {
+    let li = element.parentElement.parentElement;
+    li.remove();
 }
